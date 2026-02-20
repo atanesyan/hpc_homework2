3eab699013ce5bc8779ca5abd0d3d531986f0e08
atanesyan <artashes_atanesyan@edu.aua.am>
2026-02-20 18:27:47 +0400

    Update task4.c

--- task4.c
+++ task4.c
@@ -18,7 +18,7 @@ bool isPrime(long long n) {
     return false;
   }
     
-  for (int i = 5; i * i < n; i += 6) {
+  for (int i = 5; i * i <= n; i += 6) {
     if ((n % i) == 0 || (n % (i + 2)) == 0) {
       return false;
     } 
